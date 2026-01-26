1671f761511d26e9fea41ab8a6794462998434d0
Silas <121701012+silassdev@users.noreply.github.com>
2026-01-26 12:25:40 +0100

    Update projectsData.ts

--- app/data/projectsData.ts
+++ app/data/projectsData.ts
@@ -6,7 +6,7 @@ export const PROJECTS: Project[] = [
         id: 'p1',
         title: 'Smart CRM',
         description: 'Manage your contacts with tags, categories, and status tracking.',
-        demoUrl: 'https://alpha.allpilar.com',
+        demoUrl: 'https://demo-crm.allpilar.xyz/',
         imgA: { src: '/micro1.PNG', alt: 'Smart CRM screenshot 1', blurDataURL: '' },
         imgB: { src: '/micro2.PNG', alt: 'Smart CRM screenshot 2', blurDataURL: '' }
     },
@@ -14,7 +14,7 @@ export const PROJECTS: Project[] = [
         id: 'p2',
         title: 'Modern Resume Builder',
         description: 'A Rich, professional resume builder application built with Next.js and modern web technologies.',
-        demoUrl: 'https://beta.allpilar.com',
+        demoUrl: 'https://demo-resume.allpilar.xyz/',
         imgA: { src: '/res1.PNG', alt: 'Resume Builder screenshot 1', blurDataURL: '' },
         imgB: { src: '/res2.PNG', alt: 'Resume Builder screenshot 2', blurDataURL: '' }
     },
@@ -22,7 +22,7 @@ export const PROJECTS: Project[] = [
         id: 'p3',
         title: 'Ecommerce Online',
         description: 'A modern e-commerce scaffold designed for a computer accessories store.',
-        demoUrl: 'https://alpha.allpilar.com',
+        demoUrl: 'https://demo-ecom.allpilar.xyz/',
         imgA: { src: '/ecom1.PNG', alt: 'Ecommerce screenshot 1', blurDataURL: '' },
         imgB: { src: '/ecom2.PNG', alt: 'Ecommerce screenshot 2', blurDataURL: '' }
     },
@@ -30,7 +30,7 @@ export const PROJECTS: Project[] = [
         id: 'p4',
         title: 'Church Management & Payment Platform',
         description: 'A robust management system for churches featurin hierarchical User roles, secure payment processing, and real-time updates.',
-        demoUrl: 'https://beta.allpilar.com',
+        demoUrl: 'https://demo-church.allpilar.xyz/',
         imgA: { src: '/micro1.PNG', alt: 'Church Management screenshot 1', blurDataURL: '' },
         imgB: { src: '/micro2.PNG', alt: 'Church Management screenshot 2', blurDataURL: '' }
     }
