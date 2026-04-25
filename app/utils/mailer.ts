7e67ab5ef41aa727c9976d2794e6a3811a4389de
Silas Tyokaha <3tyokaha@gmail.com>
2026-04-25 01:56:50 +0100

    feat: implement nodemailer utility for email and admin notifications

--- app/utils/mailer.ts
+++ app/utils/mailer.ts
@@ -2,10 +2,11 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
+const port = Number(process.env.SMTP_PORT || 587)
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT || 587),
-    secure: false,
+    port,
+    secure: port === 465,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
